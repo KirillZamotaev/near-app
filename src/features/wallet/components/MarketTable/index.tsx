c4f00397e1f8d19804c7504311e66ba6d4744cf5
kirilllogrus <k.zamotaev@gateline.ru>
2022-08-09 02:41:51 +0300

    added renders for table

--- src/features/wallet/components/MarketTable/index.tsx
+++ src/features/wallet/components/MarketTable/index.tsx
@@ -1,6 +1,11 @@
 import { Table } from 'antd';
 import { useTableMarketData } from 'features/market';
+import { utils } from 'near-api-js';
 import { useMemo } from 'react';
+
+const renderSize = (value: string) => <span>{value}</span>; // utils.format.formatNearAmount(value);
+
+const render = (value: string) => <span>{value}</span>;
 
 export const MarketTable = () => {
   const { data, isLoading } = useTableMarketData();
@@ -10,29 +15,36 @@ export const MarketTable = () => {
       {
         title: 'Price',
         dataIndex: 'price',
-        key: 'price',
-        render: (text: string) => <span>{text}</span>,
+        render,
       },
       {
         title: 'Size',
-        dataIndex: 'size',
-        key: 'size',
-        render: (text: string) => <span>{text}</span>,
+        dataIndex: 'quantity',
+        render: renderSize,
       },
       {
         title: 'Total',
-        dataIndex: 'total',
-        key: 'total',
-        render: (text: string) => <span>{text}</span>,
+        dataIndex: 'quantity',
+        render,
       },
     ],
     []
   );
 
   return (
-    <div className="MarketTable">      
-      <Table loading={isLoading} dataSource={data.ask_orders} columns={columns} />
-      <Table loading={isLoading} dataSource={data.bid_orders} columns={columns} />
+    <div className="MarketTable">
+      <h3>Ask orders</h3>
+      <Table
+        loading={isLoading}
+        dataSource={data.ask_orders}
+        columns={columns}
+      />
+      <h3>Bid orders</h3>
+      <Table
+        loading={isLoading}
+        dataSource={data.bid_orders}
+        columns={columns}
+      />
     </div>
   );
 };
